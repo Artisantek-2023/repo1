3e93736e462a645faaeb6ad6bc58e2d2ada316c2
Yashwinraj <yashwinraj94@gmail.com>
2023-02-22 13:40:01 +0000

    If password is not updated user will be deleted

--- user_add.sh
+++ user_add.sh
@@ -9,6 +9,11 @@ if [ $? -eq 0 ]; then
 echo "Username already exists"
 else
 sudo useradd $username
-sudo passwd $username 
+sudo passwd $username
+if [ $? -ne 0 ]; then
+sudo userdel -r $username
+echo "unable to update password hence deleted user"
+else
 echo "user added successfully"
+fi
 fi
